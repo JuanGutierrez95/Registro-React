1b8e7bfa8ee329d549adc2948d6a4c10491113fb
JuanGutierrez95 <juan.gutierrez19958@gmail.com>
2023-11-18 22:59:29 -0300

    trabajando en el componente Registro.jsx

--- src/components/Registro.jsx
+++ src/components/Registro.jsx
@@ -2,20 +2,37 @@ const Registro = () => {
   return (
     <div>
       <form>
-        <label htmlFor="nombre" className="formLabel">
-          Nombre:
-        </label>
-        <input type="text" id="nombre" placeholder="Nombre" required></input>
-        <label>Apellido:</label>
-        <input type="text" id="apellido" placeholder="Apellido" required/>
-        <label htmlFor="email">Email:</label>
-        <input type="text" id="email" placeholder="Email" required/>
-        <label htmlFor="tel">Teléfono:</label>
-        <input type="text" id="tel" placeholder="Teléfono" required/>
-        <label htmlFor="password">Password</label>
-        <input type="text" id="password" placeholder="Password" required/>
-        <label htmlFor="confirmar">Confirmar password:</label>
-        <input type="text" id="confirmar" placeholder="Confirmar Password" required/>
+        <div>
+          <label htmlFor="nombre" className="formLabel">
+            Nombre:
+          </label>
+          <input type="text" id="nombre" placeholder="Nombre" required></input>
+        </div>
+        <div>
+          <label>Apellido:</label>
+          <input type="text" id="apellido" placeholder="Apellido" required />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input type="text" id="email" placeholder="Email" required />
+        </div>
+        <div>
+          <label htmlFor="tel">Teléfono:</label>
+          <input type="text" id="tel" placeholder="Teléfono" required />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="text" id="password" placeholder="Password" required />
+        </div>
+        <div>
+          <label htmlFor="confirmar">Confirmar password:</label>
+          <input
+            type="text"
+            id="confirmar"
+            placeholder="Confirmar Password"
+            required
+          />
+        </div>
       </form>
     </div>
   );
