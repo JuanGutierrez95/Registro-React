e6a5deb2f55c6b1f9e37b6b1137fc10c398665d9
JuanGutierrez95 <juan.gutierrez19958@gmail.com>
2023-11-18 23:44:40 -0300

    agregando estilos

--- src/components/Registro.jsx
+++ src/components/Registro.jsx
@@ -1,39 +1,82 @@
 const Registro = () => {
   return (
-    <div>
-      <form>
-        <div>
-          <label htmlFor="nombre" className="formLabel">
-            Nombre:
-          </label>
-          <input type="text" id="nombre" placeholder="Nombre" required></input>
-        </div>
-        <div>
-          <label>Apellido:</label>
-          <input type="text" id="apellido" placeholder="Apellido" required />
-        </div>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input type="text" id="email" placeholder="Email" required />
-        </div>
-        <div>
-          <label htmlFor="tel">Teléfono:</label>
-          <input type="text" id="tel" placeholder="Teléfono" required />
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input type="text" id="password" placeholder="Password" required />
-        </div>
-        <div>
-          <label htmlFor="confirmar">Confirmar password:</label>
-          <input
-            type="text"
-            id="confirmar"
-            placeholder="Confirmar Password"
-            required
-          />
-        </div>
-      </form>
+    <div className="d-flex justify-content-center align-items-center">
+      <div className="card p-2" style={{ width: "400px" }}>
+        <form>
+          <div className="mb-2">
+            <label htmlFor="nombre" className="form-label">
+              Nombre:
+            </label>
+            <input
+              type="text"
+              id="nombre"
+              placeholder="Nombre"
+              required
+              className="form-control"
+            />
+          </div>
+          <div className="mb-2">
+            <label htmlFor="apellido" className="form-label">
+              Apellido:
+            </label>
+            <input
+              type="text"
+              id="apellido"
+              placeholder="Apellido"
+              required
+              className="form-control"
+            />
+          </div>
+          <div className="mb-2">
+            <label htmlFor="email" className="form-label">
+              Email:
+            </label>
+            <input
+              type="text"
+              id="email"
+              placeholder="Email"
+              required
+              className="form-control"
+            />
+          </div>
+          <div className="mb-2">
+            <label htmlFor="tel" className="form-label">
+              Teléfono:
+            </label>
+            <input
+              type="text"
+              id="tel"
+              placeholder="Teléfono"
+              required
+              className="form-control"
+            />
+          </div>
+          <div className="mb-2">
+            <label htmlFor="password" className="form-label">
+              Password:
+            </label>
+            <input
+              type="text"
+              id="password"
+              placeholder="Password"
+              required
+              className="form-control"
+            />
+          </div>
+          <div>
+            <label htmlFor="confirmar" className="form-label">
+              Confirmar password:
+            </label>
+            <input
+              type="text"
+              id="confirmar"
+              placeholder="Confirmar Password"
+              required
+              className="form-control"
+            />
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
